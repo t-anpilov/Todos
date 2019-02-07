26055703771b2a15e6afb1329bcc8a52cdca4cf4
Taras Anpilov <t.anpilov@gmail.com>
2019-02-07 12:39:35 +0200

    still working app..

--- src/List.js
+++ src/List.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ToDoItem from './ToDoItem';
 import todosData from './todosData';
 import './app.css';
 
-function List() {
-    const todoItems = todosData.map((item) => <ToDoItem key={item.id} item={item}/>)
-    return (
-        <div className="list_block">
-            {todoItems}
-        </div>    
-    )
+
+class List extends Component {
+    constructor(props){
+        super(props)
+    }
+        
+    render() {
+        const todoItems = todosData.map((item) => <ToDoItem key={item.id} item={item}/>)
+        return (
+            <div className="list_block">
+                {todoItems}
+            </div>    
+        )
+    }    
 }
 
 export default List
